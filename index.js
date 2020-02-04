6cae7affccd836628d21420a3028151577d85ab5
YMP <patelymp007@gmail.com>
2020-02-04 18:40:52 -0500

    Database Connected

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const connectDB = require('./config/connectDB');
+
+const app = express();
+
+
+//database Connection
+connectDB();
+//set a middleware to parse data
+app.use(express.json());
+
+// app.use('/api/tasks',taskRouter);
+// app.use('/api/users',userRouter);
+
+
+app.listen(5000);
